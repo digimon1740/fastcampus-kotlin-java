dc44eca8e0f8aa0f94091f36a73a9b3750bfc234
sanghoon <digimon.1740@gmail.com>
2022-06-02 23:33:32 +0900

    init commit

---
+++ kotlin-java-spring/src/main/java/com/fastcampus/kotlinjavaspring/KotlinJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.kotlinjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotlinJavaSpringApplication.class, args);
+    }
+
+}
---
+++ kotlin-java-spring/src/test/java/com/fastcampus/kotlinjavaspring/KotlinJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.kotlinjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
